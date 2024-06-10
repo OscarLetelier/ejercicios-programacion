a9d9e5f0b2a6a7fe29ceeb2bad15b3bc57ea944a
Oscar Letelier <oscar.letelier@outlook.com>
2024-06-10 09:30:52 -0400

    Sumar Numeros

--- Ejercicios/Nivel-1/01-sumar-Dos-Números/sumar.js
+++ Ejercicios/Nivel-1/01-sumar-Dos-Números/sumar.js
@@ -1,6 +1,12 @@
 // Escribir una Función que reciba dos números y Retorne una Suma.
 
 
-function sumar (x , y) {
 
+let x = 2;
+let y = 2;
+
+function sumar (x , y) {
+    return x + y;
 };
+
+console.log(sumar(x, y));
